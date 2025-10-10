5ff2b0ebf3ce8ac11bdeb1efef8da75f09ed01be
tavstal <tavstal@gmail.com>
2025-10-10 20:18:07 +0200

    Enhance Nexus class with proxy initialization and shutdown handling, including command and event listener registration

--- proxy/src/main/java/io/github/tavstaldev/nexus/Nexus.java
+++ proxy/src/main/java/io/github/tavstaldev/nexus/Nexus.java
@@ -1,14 +1,22 @@
 package io.github.tavstaldev.nexus;
 
 import com.google.inject.Inject;
-import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.proxy.ProxyShutdownEvent;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.scheduler.ScheduledTask;
-import org.slf4j.Logger;
+import io.github.tavstaldev.nexus.config.*;
+import io.github.tavstaldev.nexus.events.*;
+import io.github.tavstaldev.nexus.logger.PluginLogger;
+import io.github.tavstaldev.nexus.managers.CommandManager;
+import io.github.tavstaldev.nexus.managers.FavIconManager;
+import org.jetbrains.annotations.NotNull;
 
 import java.nio.file.Path;
+import java.util.Set;
 
 @Plugin(
     id = "nexusproxy",
@@ -21,18 +29,91 @@ public class Nexus {
     public static Nexus plugin;
     private final ProxyServer proxy;
     private final Path dataFolder;
-    private ScheduledTask reportTask;
-    private ScheduledTask announcementsTask;
-    @Inject private Logger logger;
+    private ScheduledTask reportTask; // TODO: implement
+    private final PluginLogger pluginLogger;
+    private final ConfigurationLoader configurationLoader;
+    private final FavIconManager favIconManager;
+    private final CommandManager commandManager;
 
-    public Nexus(ProxyServer proxy, Path dataFolder) {
+    @Inject
+    public Nexus(ProxyServer proxy, @DataDirectory @NotNull Path dataFolder) {
         plugin = this;
         this.proxy = proxy;
         this.dataFolder = dataFolder;
-
+        pluginLogger = new PluginLogger();
+        configurationLoader = new ConfigurationLoader();
+        favIconManager = new FavIconManager();
+        commandManager = new CommandManager();
     }
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
+        try {
+            configurationLoader.loadAll();
+            favIconManager.loadIcons();
+
+            commandManager.registerCommands();
+            this.registerListeners();
+
+            pluginLogger.ok("nexusProxy v1.0.0 has been enabled!");
+        }
+        catch (Exception ex) {
+            pluginLogger.error("An error occurred while enabling nexusProxy:");
+            pluginLogger.error(ex);
+            commandManager.unregisterCommands();
+        }
+    }
+
+    @Subscribe
+    public void onProxyShutdown(ProxyShutdownEvent event) {
+        if (this.reportTask != null)
+            this.reportTask.cancel();
+        commandManager.unregisterCommands();
+        pluginLogger.ok("nexusProxy v1.0.0 has been disabled!");
+    }
+
+    private void registerListeners() {
+        new CommandEventListener().register();
+        new ChatEventListener().register();
+        new PreConnectEventListener().register();
+        new ProxyPingEventListener().register();
+        new ConnectEventListener().register();
+        new DisconnectEventListener().register();
+    }
+
+    public ProxyServer getProxy() {
+        return proxy;
+    }
+
+    public Path getDataFolder() {
+        return dataFolder;
+    }
+
+    public PluginLogger getLogger() {
+        return pluginLogger;
+    }
+
+    public ConfigurationLoader getConfigurationLoader() {
+        return configurationLoader;
+    }
+
+    public FavIconManager getFavIconManager() {
+        return favIconManager;
+    }
+
+    public Settings getConfig() {
+        return configurationLoader.getSettings();
+    }
+
+    public Messages getMessages() {
+        return configurationLoader.getMessages();
+    }
+
+    public MaintenanceSettings getMaintenanceSettings() {
+        return configurationLoader.getMaintenanceSettings();
+    }
+
+    public Set<Report> getReports() {
+        return configurationLoader.getReports();
     }
 }
