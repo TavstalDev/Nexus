12f66280d0c0cc87fd6ca87d621002f1a17b7b56
tavstal <tavstal@gmail.com>
2025-10-11 22:32:19 +0200

    Add overloaded sendRichMsg methods to support CommandSource and parameterized messages

--- proxy/src/main/java/io/github/tavstaldev/nexus/util/MessageUtil.java
+++ proxy/src/main/java/io/github/tavstaldev/nexus/util/MessageUtil.java
@@ -1,5 +1,6 @@
 package io.github.tavstaldev.nexus.util;
 
+import com.velocitypowered.api.command.CommandSource;
 import com.velocitypowered.api.proxy.Player;
 import io.github.tavstaldev.nexus.Nexus;
 import org.intellij.lang.annotations.RegExp;
@@ -15,6 +16,29 @@ public class MessageUtil {
     }
 
     public static void sendRichMsg(Player player, String message, Map<String, Object> parameters) {
+        String rawMessage = message;
+
+        // Get the keys
+        var keys = parameters.keySet();
+        for (@RegExp var dirKey : keys) {
+            @RegExp String finalKey;
+            if (dirKey.startsWith("%"))
+                finalKey = dirKey;
+            else
+                finalKey = "%" + dirKey + "%";
+            rawMessage = rawMessage.replace(finalKey, parameters.get(dirKey).toString());
+        }
+
+        sendRichMsg(player, rawMessage);
+    }
+
+    public static void sendRichMsg(CommandSource player, String message) {
+        message = message.replace("%prefix%", Nexus.plugin.getConfig().getPrefix());
+
+        player.sendMessage(ChatUtil.translateColors(message, true));
+    }
+
+    public static void sendRichMsg(CommandSource player, String message, Map<String, Object> parameters) {
         String rawMessage = message;
 
         // Get the keys
