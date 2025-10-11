111f008ba77ffa0abcf2d1f98670f74e0e4a3c7a
tavstal <tavstal@gmail.com>
2025-10-11 22:35:35 +0200

    Implement StaffListCommand to display online staff with permission checks

--- proxy/src/main/java/io/github/tavstaldev/nexus/command/staff/StaffListCommand.java
+++ proxy/src/main/java/io/github/tavstaldev/nexus/command/staff/StaffListCommand.java
@@ -1,4 +1,49 @@
 package io.github.tavstaldev.nexus.command.staff;
 
-public class StaffListCommand {
+import io.github.tavstaldev.nexus.Nexus;
+import io.github.tavstaldev.nexus.command.CommandBase;
+import io.github.tavstaldev.nexus.util.MessageUtil;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+public final class StaffListCommand extends CommandBase {
+    public StaffListCommand() {
+        super("stafflist",
+                "",
+                "Shows the list of online staffs.",
+                "nexus.command.stafflist",
+                new String[]{"sl", "onlinestaff", "onlinestaffs"}
+        );
+    }
+
+    @Override
+    public void execute(final Invocation invocation) {
+        var source = invocation.source();
+        if (!source.hasPermission(this.permission)) {
+            MessageUtil.sendRichMsg(source, Nexus.plugin.getMessages().getGeneralNoPermission());
+            return;
+        }
+
+        var header = Nexus.plugin.getMessages().getStaffListHeader();
+        var footer = Nexus.plugin.getMessages().getStaffListFooter();
+        var staffs = Nexus.plugin.getStaffManager().getOnlineStaff();
+        if (staffs.isEmpty()) {
+            MessageUtil.sendRichMsg(source, header);
+            MessageUtil.sendRichMsg(source, Nexus.plugin.getMessages().getStaffListNoStaff());
+            MessageUtil.sendRichMsg(source, footer);
+            return;
+        }
+
+        MessageUtil.sendRichMsg(source, header);
+        var staffEntry = Nexus.plugin.getMessages().getStaffListFormat();
+        for (var staff : staffs) {
+            MessageUtil.sendRichMsg(source, staffEntry, Map.of(
+                    "player", staff.getUsername(),
+                    "server", staff.getCurrentServer().isPresent() ? staff.getCurrentServer().get().getServerInfo().getName() : "?????"
+            ));
+        }
+        MessageUtil.sendRichMsg(source, footer);
+    }
 }
