996a1accf401b443c9e122cc1bbdee753a52abd6
tavstal <tavstal@gmail.com>
2025-10-11 22:35:30 +0200

    Refactor CommandBase to improve command registration and add permission checking

--- proxy/src/main/java/io/github/tavstaldev/nexus/command/CommandBase.java
+++ proxy/src/main/java/io/github/tavstaldev/nexus/command/CommandBase.java
@@ -1,6 +1,7 @@
 package io.github.tavstaldev.nexus.command;
 
 import com.velocitypowered.api.command.CommandManager;
+import com.velocitypowered.api.command.CommandMeta;
 import com.velocitypowered.api.command.SimpleCommand;
 import io.github.tavstaldev.nexus.Nexus;
 
@@ -11,7 +12,7 @@ public abstract class CommandBase implements SimpleCommand {
     protected String permission;
     protected String[] aliases;
 
-    public CommandBase(String baseCommand, String syntax, String description, String permission, String... aliases) {
+    public CommandBase(String baseCommand, String syntax, String description, String permission, String[] aliases) {
         this.baseCommand = baseCommand;
         this.syntax = syntax;
         this.description = description;
@@ -21,18 +22,25 @@ public abstract class CommandBase implements SimpleCommand {
 
     public void register() {
         CommandManager commandManager = Nexus.plugin.getProxy().getCommandManager();
-        commandManager.register(commandManager.metaBuilder(baseCommand)
+        CommandMeta commandMeta = commandManager.metaBuilder(baseCommand)
                 .aliases(aliases)
-                .build(), this);
-        Nexus.plugin.getLogger().debug("Registered command: " + baseCommand);
+                .plugin(Nexus.plugin)
+                .build();
+        commandManager.register(commandMeta, this);
+        Nexus.plugin.getLogger().info("Registered command: " + baseCommand);
     }
 
     public void unregister() {
         CommandManager commandManager = Nexus.plugin.getProxy().getCommandManager();
         commandManager.unregister(baseCommand);
-        Nexus.plugin.getLogger().debug("Unregistered command: " + baseCommand);
+        Nexus.plugin.getLogger().info("Unregistered command: " + baseCommand);
     }
 
     @Override
-    public abstract void execute(Invocation invocation);
+    public boolean hasPermission(final Invocation invocation) {
+        if (permission == null || permission.isEmpty()) {
+            return true;
+        }
+        return invocation.source().hasPermission(permission);
+    }
 }
