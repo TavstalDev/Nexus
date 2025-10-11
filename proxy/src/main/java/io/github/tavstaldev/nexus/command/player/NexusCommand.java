deaffb36f3c74f1f727c596ce1cc81d25e0658e9
tavstal <tavstal@gmail.com>
2025-10-11 22:35:42 +0200

    Implement NexusCommand to handle main command execution and send versioned messages

--- proxy/src/main/java/io/github/tavstaldev/nexus/command/player/NexusCommand.java
+++ proxy/src/main/java/io/github/tavstaldev/nexus/command/player/NexusCommand.java
@@ -1,4 +1,31 @@
 package io.github.tavstaldev.nexus.command.player;
 
-public class NexusCommand {
+import io.github.tavstaldev.nexus.Nexus;
+import io.github.tavstaldev.nexus.NexusConstants;
+import io.github.tavstaldev.nexus.command.CommandBase;
+import io.github.tavstaldev.nexus.util.MessageUtil;
+
+import java.util.Map;
+
+public class NexusCommand extends CommandBase {
+    public NexusCommand() {
+        super("nexus",
+                "",
+                "Main command for Nexus plugin.",
+                "",
+                new String[]{"nex"}
+        );
+    }
+
+    @Override
+    public void execute(final Invocation invocation) {
+        var source = invocation.source();
+        var header = Nexus.plugin.getMessages().getNexusInfoHeader();
+        var body = Nexus.plugin.getMessages().getNexusInfoContent();
+        var footer = Nexus.plugin.getMessages().getNexusInfoFooter();
+        MessageUtil.sendRichMsg(source, header);
+        MessageUtil.sendRichMsg(source, body, Map.of(
+                "version", NexusConstants.VERSION));
+        MessageUtil.sendRichMsg(source, footer);
+    }
 }
