0359712fa32ce8372939eff8efce61f6fd6920aa
tavstal <tavstal@gmail.com>
2025-10-11 22:36:07 +0200

    Implement HubCommand to teleport players to the hub/lobby server

--- proxy/src/main/java/io/github/tavstaldev/nexus/command/player/HubCommand.java
+++ proxy/src/main/java/io/github/tavstaldev/nexus/command/player/HubCommand.java
@@ -1,4 +1,28 @@
 package io.github.tavstaldev.nexus.command.player;
 
-public class HubCommand {
+import com.velocitypowered.api.proxy.Player;
+import io.github.tavstaldev.nexus.Nexus;
+import io.github.tavstaldev.nexus.command.CommandBase;
+import io.github.tavstaldev.nexus.util.MessageUtil;
+
+public class HubCommand extends CommandBase {
+    public HubCommand() {
+        super("hub",
+                "",
+                "Teleports you to the hub/lobby server.",
+                "",
+                new String[]{"lobby"}
+        );
+    }
+
+    @Override
+    public void execute(final Invocation invocation) {
+        var source = invocation.source();
+        if (!(source instanceof Player player)) {
+            MessageUtil.sendRichMsg(source, Nexus.plugin.getMessages().getGeneralPlayerOnly());
+            return;
+        }
+
+        // TODO
+    }
 }
