6fec95886cd264297f1a31161718003dbc6026fd
tavstal <tavstal@gmail.com>
2025-10-12 16:32:31 +0200

    Enhance LobbyServerManager to track server status and manage lobby servers dynamically

--- proxy/src/main/java/io/github/tavstaldev/nexus/managers/LobbyServerManager.java
+++ proxy/src/main/java/io/github/tavstaldev/nexus/managers/LobbyServerManager.java
@@ -1,20 +1,41 @@
 package io.github.tavstaldev.nexus.managers;
 
 import com.velocitypowered.api.proxy.server.RegisteredServer;
+import io.github.tavstaldev.nexus.Nexus;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class LobbyServerManager {
-    private final Set<RegisteredServer> lobbyServers = new HashSet<>();
+    private final Set<String> serversToTrack;
+    private final HashMap<RegisteredServer, Boolean> lobbyServers = new HashMap<>();
 
-    public void registerLobbyServer(RegisteredServer server) {
-        lobbyServers.add(server);
+    public LobbyServerManager(Set<String> serversToTrack) {
+        this.serversToTrack = serversToTrack;
+        for (String serverName : serversToTrack) {
+            Nexus.plugin.getProxy().getServer(serverName).ifPresent(server -> lobbyServers.put(server, true));
+        }
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(2);
+        executorService.scheduleAtFixedRate(this::checkServerStatus, 0L, 500L, TimeUnit.MILLISECONDS);
     }
 
-    public void unregisterLobbyServer(RegisteredServer server) {
-        lobbyServers.removeIf(x -> x.equals(server));
+    private void checkServerStatus() {
+        lobbyServers.keySet().forEach(server ->
+                server.ping().thenAcceptAsync(result ->
+                        lobbyServers.put(server, result != null)
+                )
+        );
+        serversToTrack.forEach(serverName ->
+                Nexus.plugin.getProxy().getServer(serverName).ifPresent(server ->
+                        lobbyServers.putIfAbsent(server, true)
+                )
+        );
     }
 
     public @Nullable RegisteredServer getLobbyServer() {
@@ -23,7 +44,11 @@ public class LobbyServerManager {
         }
         int currentPlayers = Integer.MAX_VALUE;
         RegisteredServer bestServer = null;
-        for (RegisteredServer server : lobbyServers) {
+        for (RegisteredServer server : lobbyServers.keySet()) {
+            if (!lobbyServers.get(server)) {
+                continue;
+            }
+
             int serverPlayers = server.getPlayersConnected().size();
             if (serverPlayers < currentPlayers) {
                 currentPlayers = serverPlayers;
@@ -31,5 +56,9 @@ public class LobbyServerManager {
             }
         }
         return  bestServer;
+    }
+
+    public boolean isLobbyServer(RegisteredServer server) {
+        return lobbyServers.containsKey(server);
     }
 }
