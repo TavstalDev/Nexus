18fdeec73c50f5fdd1b05a0762b26bbf2f8bd927
tavstal <tavstal@gmail.com>
2025-10-12 21:30:44 +0200

    Implement MaintenanceCommand to manage player maintenance status with add, remove, list, on, and off subcommands (WIP)

--- proxy/src/main/java/io/github/tavstaldev/nexus/command/admin/MaintenanceCommand.java
+++ proxy/src/main/java/io/github/tavstaldev/nexus/command/admin/MaintenanceCommand.java
@@ -1,4 +1,163 @@
 package io.github.tavstaldev.nexus.command.admin;
 
-public class MaintenanceCommand {
+import com.velocitypowered.api.proxy.Player;
+import io.github.tavstaldev.nexus.Nexus;
+import io.github.tavstaldev.nexus.command.CommandBase;
+import io.github.tavstaldev.nexus.config.maintenance.MaintenancePlayer;
+import io.github.tavstaldev.nexus.util.MessageUtil;
+
+import java.util.Map;
+
+public class MaintenanceCommand extends CommandBase {
+    public MaintenanceCommand() {
+        super("maintenance",
+                "<add|remove|list|on|off>",
+                "Toggles maintenance mode on or off.",
+                "nexus.command.maintenance",
+                new String[]{"maintain"}
+        );
+    }
+
+    // TODO
+
+    @Override
+    public void execute(final Invocation invocation) {
+        var source = invocation.source();
+        if (invocation.arguments().length == 0) {
+            MessageUtil.sendRichMsg(source, Nexus.plugin.getMessages().getGeneralCommandSyntax(), Map.of(
+                    "syntax", this.syntax,
+                    "command", this.baseCommand
+            ));
+            return;
+        }
+
+        var subcommand = invocation.arguments()[0].toLowerCase();
+        var config = Nexus.plugin.getMaintenanceSettings();
+        switch (subcommand) {
+            case "add": {
+                if (invocation.arguments().length != 2) {
+                    MessageUtil.sendRichMsg(source, Nexus.plugin.getMessages().getGeneralCommandSyntax(), Map.of(
+                            "syntax", " add <player>",
+                            "command", this.baseCommand
+                    ));
+                    return;
+                }
+
+                var playerName = invocation.arguments()[1];
+                if (config.isPlayerAllowed(playerName)) {
+                    MessageUtil.sendRichMsg(source, Nexus.plugin.getMessages().getMaintenancePlayerAlreadyAllowed(), Map.of(
+                            "player", playerName
+                    ));
+                    return;
+                }
+
+                if (source instanceof Player adder) {
+                    config.addPlayer(playerName, adder);
+                } else {
+                    config.addPlayer(playerName);
+                }
+                Nexus.plugin.getConfigurationLoader().saveMaintenanceConfig();
+                MessageUtil.sendRichMsg(source, Nexus.plugin.getMessages().getMaintenancePlayerAdded(), Map.of(
+                        "player", playerName
+                ));
+                break;
+            }
+            case "remove": {
+                if (invocation.arguments().length != 2) {
+                    MessageUtil.sendRichMsg(source, Nexus.plugin.getMessages().getGeneralCommandSyntax(), Map.of(
+                            "syntax", " remove <player>",
+                            "command", this.baseCommand
+                    ));
+                    return;
+                }
+
+                var playerName = invocation.arguments()[1];
+                if (!config.isPlayerAllowed(playerName)) {
+                    MessageUtil.sendRichMsg(source, Nexus.plugin.getMessages().getMaintenancePlayerNotAllowed(), Map.of(
+                            "player", playerName
+                    ));
+                    return;
+                }
+
+                config.removePlayer(playerName);
+                Nexus.plugin.getConfigurationLoader().saveMaintenanceConfig();
+                MessageUtil.sendRichMsg(source, Nexus.plugin.getMessages().getMaintenancePlayerRemoved(), Map.of(
+                        "player", playerName
+                ));
+                break;
+            }
+            case "list": {
+                int page = 1;
+                if (invocation.arguments().length >= 2) {
+                    try {
+                        page = Integer.parseInt(invocation.arguments()[1]);
+                        if (page < 1)
+                            page = 1;
+                    } catch (NumberFormatException e) {
+                        MessageUtil.sendRichMsg(source, Nexus.plugin.getMessages().getGeneralInvalidNumber(), Map.of(
+                                "number", invocation.arguments()[1]
+                        ));
+                        return;
+                    }
+                }
+
+                var allowedPlayers = config.getPlayers().toArray(new MaintenancePlayer[0]);
+                if (allowedPlayers.length == 0) {
+                    MessageUtil.sendRichMsg(source, Nexus.plugin.getMessages().getMaintenanceNoAllowedPlayers());
+                    return;
+                }
+
+                int itemsPerPage = 15;
+                int totalPages = (int) Math.ceil((double) allowedPlayers.size() / itemsPerPage);
+                if (page > totalPages)
+                    page = totalPages;
+
+                for (int i = 0; i < itemsPerPage; i++) {
+                    int index = (page - 1) * itemsPerPage + i;
+                    if (index >= allowedPlayers.size())
+                        break;
+                    String playerName = allowedPlayers[index].getName();
+                    MessageUtil.sendRichMsg(source, Nexus.plugin.getMessages().getMaintenanceAllowedPlayerFormat(), Map.of(
+                            "player", playerName
+                    ));
+                }
+                break;
+            }
+            case "on": {
+                if (config.isEnabled()) {
+                    MessageUtil.sendRichMsg(source, Nexus.plugin.getMessages().getMaintenanceAlreadyOn());
+                    return;
+                }
+
+                config.setEnabled(true);
+                if (source instanceof Player player) {
+                    if (!config.isPlayerAllowed(player)) {
+                        config.addPlayer(player, player);
+                    }
+                }
+                Nexus.plugin.getConfigurationLoader().saveMaintenanceConfig();
+                MessageUtil.sendRichMsg(source, Nexus.plugin.getMessages().getMaintenanceEnabled());
+                break;
+            }
+            case "off": {
+                if (!config.isEnabled()) {
+                    MessageUtil.sendRichMsg(source, Nexus.plugin.getMessages().getMaintenanceAlreadyOff());
+                    return;
+                }
+
+                config.setEnabled(false);
+                Nexus.plugin.getConfigurationLoader().saveMaintenanceConfig();
+                MessageUtil.sendRichMsg(source, Nexus.plugin.getMessages().getMaintenanceDisabled());
+                break;
+            }
+            default:
+            {
+                MessageUtil.sendRichMsg(source, Nexus.plugin.getMessages().getGeneralCommandSyntax(), Map.of(
+                        "syntax", this.syntax,
+                        "command", this.baseCommand
+                ));
+                break;
+            }
+        }
+    }
 }
