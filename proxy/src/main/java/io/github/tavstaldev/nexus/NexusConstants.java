739383c052c6503d6a222ebf4b104ca19a4b2fe9
tavstal <tavstal@gmail.com>
2025-10-23 13:20:59 +0200

    Bump version to 1.0.1 in NexusConstants, version.yml, and gradle.properties

--- proxy/src/main/java/io/github/tavstaldev/nexus/NexusConstants.java
+++ proxy/src/main/java/io/github/tavstaldev/nexus/NexusConstants.java
@@ -1,6 +1,6 @@
 package io.github.tavstaldev.nexus;
 
 public class NexusConstants {
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String STAFF_PERMISSION = "nexus.staff";
 }
